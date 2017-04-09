94b93d815023f506738f8a2cc54fc908095c125e
Erik Vanderlei Fernandes <erik.vanderlei.programador@outlook.com>
2017-04-08 21:09:15 -0300

    Add symfony var-dumper

--- app/utils.php
+++ app/utils.php
@@ -1,11 +1,7 @@
 <?php
 
 if (!function_exists('dd')) {
-    // Make this with symfony dumper
     function dd($expression) {
-        echo "<pre>";
-        var_dump($expression);
-        echo "</pre>";
-        die();
+        dump($expression); die();
     }
 }
